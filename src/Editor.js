38e6a8bb3038a6f4e2f58b036792cc608fa136c0
Jenssen Lee <30688276+jenlky@users.noreply.github.com>
2025-01-14 07:54:04 +0000

    Add div wrapper and toolbar class name

--- src/Editor.js
+++ src/Editor.js
@@ -1,8 +1,15 @@
 export default function Editor({ text, setText }) {
   return (
-    <textarea
-      id="editor"
-      onChange={(event) => setText(event.target.value)}
-    >{text}</textarea>
+    <div className="editorWrap">
+      <div className="toolbar">
+        <i class="fa fa-free-code-camp" title="no-stack-dub-sack"></i>
+        Editor
+        <i class="fa fa-arrows-alt"></i>
+      </div>
+      <textarea
+        id="editor"
+        onChange={(event) => setText(event.target.value)}
+      >{text}</textarea>
+    </div>
   );
 }
--- src/Previewer.js
+++ src/Previewer.js
@@ -2,8 +2,15 @@ import Markdown from 'react-markdown'
 
 export default function Previewer({ text }) {
   return (
-    <div id="previewer">
-      <Markdown>{text}</Markdown>
+    <div className="previewWrap">
+      <div className='toolbar'>
+        <i class="fa fa-free-code-camp" title="no-stack-dub-sack"></i>
+        Previewer
+        <i class="fa fa-arrows-alt"></i>
+      </div>
+      <div id='preview'>
+        <Markdown>{text}</Markdown>
+      </div>
     </div>
   );
 }
